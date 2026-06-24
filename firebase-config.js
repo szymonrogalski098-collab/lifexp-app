18cc3eeb8486b5c3bad587bd53d168f207e6ec62
Szymon Rogalski <szymonrogalski098@gmail.com>
2026-06-24 18:02:50 +0200

    init

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// TWÓJ FIREBASE CONFIG — nie udostępniaj publicznie
+const firebaseConfig = {
+  apiKey: "AIzaSyD7Nk7rZydzSA5AdKPJpn0Jm18_LvpAsS4",
+  authDomain: "faiobaj4.firebaseapp.com",
+  projectId: "faiobaj4",
+  storageBucket: "faiobaj4.firebasestorage.app",
+  messagingSenderId: "256487131449",
+  appId: "1:256487131449:web:9161866d149951b1e580e8"
+};
